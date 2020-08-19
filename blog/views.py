7ecf8931325e053f7cbdf5d6898926582d7dec07
Natalia <naatkaa132@gmail.com>
2020-08-19 21:14:46 +0100

    adding a post model and creating a list of posts and its details

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post_list.html'
+
+
+def post_list(request):
+    posts = Post.published.all()
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post_list.html',
+                  {'posts': posts,
+                   'page': page})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status="published", publish__year=year, publish__month=month, publish__day=day)
+    return render(request, 'blog/post_detail.html', {'post': post})
